6045853324267206eaa7f7253ee52610f35be88f
w17ch3r <w17ch3r@stopspam.com>
2022-11-09 16:15:57 +0000

    Решение задачи 2.02

---
+++ solutions/section2/exercises/02/solution.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+   printf("Parkinson's Law:\nWork expands so as too ");
+   printf("fill the time\n");
+   printf("available for its completion.\n");
+   return 0;
+}
